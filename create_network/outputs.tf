1881d033adc3b9584c4b1a25213917ef74df7c2b
gsophy <georgesophy@gmail.com>
2019-08-06 15:24:16 -0400

    fix outputs.tf

--- create_network/outputs.tf
+++ create_network/outputs.tf
@@ -1,3 +1,3 @@
 output "user_id" {value = "${module.user.user_id}"}
 output "User Name" {value = "${var.user_name}"}
-output "UserUIPassword" {sensitive = false value = ["${module.user.UserUIPassword}"]}
+#output "UserUIPassword" {sensitive = false value = ["${module.user.UserUIPassword}"]}
